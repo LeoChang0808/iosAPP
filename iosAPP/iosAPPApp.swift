42866dafae0b3bfd1f2b85611cd4dec48f348dbf
leo chang <leochang0808@gmail.com>
2023-10-01 18:36:55 +0800

    Initial Commit

---
+++ iosAPP/iosAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAPPApp.swift
+//  iosAPP
+//
+//  Created by LEO CHANG on 2023/10/1.
+//
+
+import SwiftUI
+
+@main
+struct iosAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
